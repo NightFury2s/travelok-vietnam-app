452a54556b19803ca07c7ccd91a0b5b1569dcdb2
FARBE ELIASs <117972059+ErykHg@users.noreply.github.com>
2022-11-24 20:09:24 +0700

    Hoang Thai Ninh 53398

---
+++ lib/Views/SplashHome/SplashHome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashHome extends StatefulWidget {
+  const SplashHome({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<SplashHome> createState() => _SplashHomeState();
+}
+
+class _SplashHomeState extends State<SplashHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
