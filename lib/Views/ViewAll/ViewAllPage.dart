0a062fee33bd40df3019b1220c8736207343249e
DangTranNgocManh53907 <89623168+NightFury2s@users.noreply.github.com>
2022-12-01 19:39:09 +0700

    Manh 53907

--- lib/Views/ViewAll/ViewAllPage.dart
+++ lib/Views/ViewAll/ViewAllPage.dart
@@ -1,18 +1,187 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class ViewAllPage extends StatefulWidget {
-  const ViewAllPage({Key? key}) : super(key: key);
+import '../../Models/Station_Travel.dart';
 
+class ViewAllPage extends StatefulWidget {
   @override
   State<ViewAllPage> createState() => _ViewAllPageState();
 }
 
 class _ViewAllPageState extends State<ViewAllPage> {
+  Text _RatingStars(int rating) {
+    String stars = '';
+    for (int i = 0; i < rating; i++) {
+      stars += '⭐';
+    }
+    return Text(stars);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('VIEW ALL PAGE'),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 18),
+        ),
+        title: RichText(
+          text: const TextSpan(children: [
+            TextSpan(
+                text: 'Popular Package',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black))
+          ]),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.all(10),
+              child: (const Text(
+                'All Popular Trip Package',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ))),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Stack(children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.fromLTRB(20, 5, 10, 10),
+                          height: 170,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(100, 20, 20, 20),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Container(
+                                        width: 120,
+                                        child: const Text(
+                                          'Santorini Island',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold),
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 2,
+                                        ),
+                                      ),
+                                      Column(
+                                        children: [
+                                          Container(
+                                            alignment: Alignment.center,
+                                            width: 40,
+                                            height: 25,
+                                            decoration: BoxDecoration(
+                                                color: Colors.blue,
+                                                borderRadius:
+                                                    BorderRadius.circular(5)),
+                                            child: const Text(
+                                              '\$820',
+                                              style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.white),
+                                            ),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Column(
+                                    children: [
+                                      Row(
+                                        children: <Widget>[
+                                          Container(
+                                              width: 200,
+                                              decoration: BoxDecoration(
+                                                  color: Colors.white,
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10)),
+                                              child: Row(
+                                                children: const [
+                                                  Icon(
+                                                    Icons.calendar_month,
+                                                    color: Colors.grey,
+                                                    size: 20,
+                                                  ),
+                                                  SizedBox(width: 2),
+                                                  Text(
+                                                    '16 July-28 July',
+                                                    style: TextStyle(
+                                                        fontSize: 14,
+                                                        color: Colors.grey),
+                                                  ),
+                                                ],
+                                              )),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    children: <Widget>[
+                                      Container(
+                                          width: 200,
+                                          decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: Row(
+                                            children: [
+                                              _RatingStars(3),
+                                              const SizedBox(width: 2),
+                                              const Text(
+                                                '4.8',
+                                                style: TextStyle(
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.w400,
+                                                    color: Colors.black),
+                                              ),
+                                            ],
+                                          )),
+                                    ],
+                                  )
+                                ]),
+                          )),
+                      Positioned(
+                        left: 15,
+                        top: 15,
+                        bottom: 15,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image(
+                            width: 100,
+                            image: AssetImage('assets/images/bg-welcome.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    ]);
+                  }))
+        ],
       ),
     );
   }
